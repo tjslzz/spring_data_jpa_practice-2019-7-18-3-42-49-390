bd0708a0fc93055e102558396ca90d0314754127
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-18 16:40:55 +0800

    [jerryLi] build : build success of flyway and the schema'

--- src/main/java/com/tw/apistackbase/ApiStackBaseApplication.java
+++ src/main/java/com/tw/apistackbase/ApiStackBaseApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiStackBaseApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiStackBaseApplication.class, args);
 	}
 }
